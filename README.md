697a378649cf5333f4ca243b5dbc815021d851ac
ayang <473033518@qq.com>
2023-03-06 09:14:02 +0800

    perf: optimized request parameter passing and judgment when packing

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## 无需秘钥，可以直接在 bob 中进行工作，如果有一天它不工作了，请联系我进行修复！
 
-> src 文件夹主要为 bob 用户开发，bob 是一款 macOS 上的翻译软件，bob 官网地址：[bob](https://bobtranslate.com/)
+> src 文件夹主要为 bob 用户开发，bob 是一款 macOS 上的翻译软件，官网地址：[bob](https://bobtranslate.com/)
 >
 > 插件下载地址：[bob-plugin-free-ali-translate-v0.0.4.bobplugin](https://github.com/bilibili-ayang/bob-plugin-free-ali-translate/releases/download/v0.0.4/bob-plugin-free-ali-translate.bobplugin)
 
--- src/main.js
+++ src/main.js
@@ -27,13 +27,6 @@ async function translate(query, completion) {
 				tgtLang,
 				domain: "general",
 			},
-			header: {
-				Cookie: "xman_us_f=x_l=1; xman_t=V6c1xatsl3xJqeIlbQ++JhI7Btqfg488sRpAoO/5Amt7JsaysHLnVdbDg3LLo7O6; acs_usuc_t=acs_rt=87ba93aba09f4703b0d8709b8873ca8c; acs_t=jlhH5HD3A+qW6FE5oN0ZUh/hPFB54QO9BW0GhyHc8Y+1+upx+iu1wenSF5t+HHAg; xman_f=NaoZ/ox90EPhyKabS/QZYOyauY+wntq5gPQIrco4qC/RWSX454dJwEfT64fwTBnms09wRIQayGfryUGc+Neg2UMBtn0dbR5xzNuNM7JgLvOE9vsD53HiDQ==; __itrace_wid=cfacd9a5-93a4-48ae-097e-3817f7932d5a; cna=jvWAHDS/rikCAcor6mg9NxST; xlly_s=1; tfstk=ciHVBQVX9R0SL5n9J82ZT2okCFeAZLFzTLr3iFsMIAXAZ7Pci37TqGbi4cjPSSf..; l=fBQEEB8RTq2QnCxhBOfZourza77TAIRfguPzaNbMi9fP_D5p5tKfB68wRvY9CnGVEsK653--PjLJB0LnBy49lTYhxJRVakp09dTnwpzHU; isg=BPX1ocu9-k7kRB7y4UMfO7XBBHevcqmE6vdiO3cammy7ThRAO8PiVX2ImBL4DsE8",
-				Origin: "https://translate.alibaba.com",
-				Referer: "https://translate.alibaba.com/",
-				"User-Agent":
-					"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-			},
 			timeout: 1000 * 60,
 		});
 
